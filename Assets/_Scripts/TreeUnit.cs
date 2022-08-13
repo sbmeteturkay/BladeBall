ceb39136b68144c134dfdaad40e2c4245c6c0bc9
sabanmeteturkay <44952253+sbmeteturkay@users.noreply.github.com>
2022-08-13 17:57:26 +0300

    Destructable tree

--- Assets/_Scripts/TreeUnit.cs
+++ Assets/_Scripts/TreeUnit.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class TreeUnit : MonoBehaviour
 {
     [SerializeField] Tree tree;
+    [SerializeField] Transform TopLeafs;
+    [SerializeField] Transform BotLeafs;
+    [SerializeField] Transform Wood;
+    [SerializeField] GameObject StaticTree;
+    private void Start()
+    {
+        tree.currentHealth = tree.health;
+        tree.treeState = TreeState.full;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Blade") { 
@@ -19,13 +28,26 @@ public class TreeUnit : MonoBehaviour
             case TreeState.full:
                 break;
             case TreeState.leafless:
+                OpenChildRigidbodys(TopLeafs);
                 break;
             case TreeState.chopped:
+                OpenChildRigidbodys(BotLeafs);
                 break;
             case TreeState.destroyed:
+                OpenChildRigidbodys(Wood);
                 break;
             default:
                 break;
+        }
+    }
+    void OpenChildRigidbodys(Transform parent)
+    {
+        StaticTree.SetActive(false);
+        parent.gameObject.transform.parent.gameObject.SetActive(true);
+        foreach (Transform child in parent)
+        {
+            child.GetComponent<Rigidbody>().isKinematic = false;
+            Destroy(child.gameObject, 2);
         }
     }
 }
