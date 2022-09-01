3b6b654fdc3b50a316d381a0aa35e862e18a02a7
sabanmeteturkay <44952253+sbmeteturkay@users.noreply.github.com>
2022-09-01 19:17:57 +0300

    sound adjustments and currency trade multipler

--- Assets/_Scripts/CurrencyTrade.cs
+++ Assets/_Scripts/CurrencyTrade.cs
@@ -1,5 +1,7 @@
 using TMPro;
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class CurrencyTrade : MonoBehaviour
 {
@@ -14,53 +16,65 @@ public class CurrencyTrade : MonoBehaviour
         giveText.text = givenAmount.ToString();
         takeText.text = takenAmount.ToString();
     }
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.name == "NPCTriggerCollider")
-        {
-          TradeCurrency(givenType, takenType, givenAmount,takenAmount);
-            tradeSound.loop = true;
-        }
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "NPCTriggerCollider")
-            tradeSound.Play();
+        {
+           StartCoroutine(TradeCurrency(givenType, takenType, givenAmount, takenAmount, 0.1f));
+        }
+            
     }
     private void OnTriggerExit(Collider other)
         {
             if (other.name == "NPCTriggerCollider")
             {
-                tradeSound.loop = false;
+            StopAllCoroutines();
             }
     }
-    void TradeCurrency(CollectType give,CollectType take, int _givenAmount,int _takenAmount)
+    IEnumerator TradeCurrency(CollectType give, CollectType take, int _givenAmount, int _takenAmount, float repeatTime = 1f)
     {
-        switch (give)
+        while (true)
         {
-            case CollectType.wood:
-                if (GameDataManager.CanSpendCoins(_givenAmount, CollectType.wood))
-                {
-                    GameDataManager.SpendCoins(_givenAmount, give);
-                    GameDataManager.AddCoins(_takenAmount, take);
-                    //SoundManager.Instance.Play(SoundManager.Sounds.tradeCoin, true, false);
-                }
-                break;
-            case CollectType.coin:
-                if (GameDataManager.CanSpendCoins(_givenAmount, CollectType.coin))
-                {
-                    GameDataManager.SpendCoins(_givenAmount, give);
-                    GameDataManager.AddCoins(_takenAmount, take);
-                    //SoundManager.Instance.Play(SoundManager.Sounds.tradeCoin, true, false);
-                }
-                
-                break;
-            case CollectType.gem:
-                break;
-            default:
-                break;
-        }
+            switch (give)
+            {
+                case CollectType.wood:
+                    if (GameDataManager.CanSpendCoins(PlayerUnit.Instance.chopper.capacity / 10, CollectType.wood))
+                    {
+                        GameDataManager.SpendCoins(PlayerUnit.Instance.chopper.capacity / 10, give);
+                        GameDataManager.AddCoins((PlayerUnit.Instance.chopper.capacity / 10) * _takenAmount, take);
+                        tradeSound.PlayOneShot(tradeSound.clip);
+                    }
+                    else if (GameDataManager.CanSpendCoins(_givenAmount, CollectType.wood))
+                    {
+                        GameDataManager.SpendCoins(_givenAmount, give);
+                        GameDataManager.AddCoins(_takenAmount, take);
+                        //SoundManager.Instance.Play(SoundManager.Sounds.tradeCoin, true, false);
+                        tradeSound.PlayOneShot(tradeSound.clip);
+                    }
 
+                    else
+                        StopAllCoroutines();
+                    break;
+                case CollectType.coin:
+                    if (GameDataManager.CanSpendCoins(_givenAmount, CollectType.coin))
+                    {
+                        GameDataManager.SpendCoins(_givenAmount, give);
+                        GameDataManager.AddCoins(_takenAmount, take);
+                        //SoundManager.Instance.Play(SoundManager.Sounds.tradeCoin, true, false);
+                        tradeSound.PlayOneShot(tradeSound.clip);
+                    }
+                    else
+                        StopAllCoroutines();
+                    break;
+                case CollectType.gem:
+                    break;
+                default:
+                    break;
+            }
+        
+        yield return new WaitForSeconds(repeatTime);
+            
+        }
 
     }
 }
